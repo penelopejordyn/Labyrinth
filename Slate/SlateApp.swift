b746430f1dc425ac2bb6a5137e162901fd57007c
Penny Marshall <pennyjlynette@gmail.com>
2025-10-27 14:29:18 -0400

    Initial Commit

---
+++ Slate/SlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlateApp.swift
+//  Slate
+//
+//  Created by Penny Marshall on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlateTests/SlateTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlateTests.swift
+//  SlateTests
+//
+//  Created by Penny Marshall on 10/27/25.
+//
+
+import Testing
+@testable import Slate
+
+struct SlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
